9089e2da40714a0d9c2efc9a5bf71e45b322e4b1
付维浪 <fuweilang@xunlei.com>
2017-12-20 16:52:26 +0800

    20171220.16:52

--- build/dev-server.js
+++ build/dev-server.js
@@ -4,11 +4,17 @@ let configBuild = require('../config/index.js');
 let path = require('path');
 
 let configBuildPath = path.resolve(__dirname, '../config/index.js');
-let outputPath = path.resolve(configBuild.dev)
+let outputPath = path.resolve(configBuild.dev);
 
 if (shell.test('-d', outputPath)) {
-  shell.rm('-rf', outputPath);
+    shell.rm('-rf', outputPath + '/*');
 }
+else{
+    shell.mkdir(outputPath);
+}
+
+console.log('localhost:' + configBuild.port)
+
 shell.exec('fis3 server stop -p ' + configBuild.port);
-shell.exec('fis3 server start --no-browse --root ' + outputPath + ' -p ' + configBuild.port);
+shell.exec('fis3 server start --root ' + outputPath + ' -p ' + configBuild.port);
 shell.exec('fis3 release -wLcd ' + outputPath);
